731565d27d93317ee9eee856c503add0ab6c5c03
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2021-12-17 14:56:31 +0700

    refactor: Refactor and add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Website tĩnh cho cá nhân
+
+## Công nghệ sử dụng
+
+- HTML5
+- CSS
+- VanillaJS
+
+## Tính năng hiện có
+
+- Fully Responsive Website
+- Toggle button on Tablet & Mobile
--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-var navToggle = $('.nav-toggle');
-var main = $('.main');
-var skillBars = $$('.skill__bar');
-var skillPercent = $$('.skill__percent');
+const navToggle = $(".nav-toggle");
+const main = $(".main");
+const skillBars = $$(".skill__bar");
+const skillPercent = $$(".skill__percent");
 
-for (var i in skillBars) {
-    skillBars[i].style.width = skillPercent[i].innerText;
+const skillBarArray = [...skillBars];
+const skillPercentArray = [...skillPercent];
+
+for (const i in skillBarArray) {
+  skillBarArray[i].style.width = skillPercentArray[i].innerText;
 }
 
 navToggle.onclick = function () {
-    var navToggleIcon = $('.nav-toggle-icon');
-    var navToggleIconActived = $('.nav-toggle-icon-actived');
-    navToggleIcon.classList.toggle('display-none');
-    navToggleIconActived.classList.toggle('display-none');
-    main.classList.toggle('nav-active')
-    document.addEventListener('scroll', function () {
-        main.classList.remove('nav-active');
-        navToggleIcon.classList.remove('display-none');
-        navToggleIconActived.classList.add('display-none');
-    });
-}
+  var navToggleIcon = $(".nav-toggle-icon");
+  var navToggleIconActived = $(".nav-toggle-icon-actived");
+  navToggleIcon.classList.toggle("display-none");
+  navToggleIconActived.classList.toggle("display-none");
+  main.classList.toggle("nav-active");
+  document.addEventListener("scroll", function () {
+    main.classList.remove("nav-active");
+    navToggleIcon.classList.remove("display-none");
+    navToggleIconActived.classList.add("display-none");
+  });
+};
